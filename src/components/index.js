da747543bf9041db12ac728eac649820ccfbe39d
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2022-04-27 20:20:30 -0300

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+import {Card, CardContent} from './Card'
+import Button from './Button'
+import Grid from './Grid'
+import Typography from './Typography'
+import Skeleton from './Skeleton'
+import Select from './Select'
+import MenuItem from './MenuItem'
+import Box from "./Box";
+
+export {
+    Button,
+    Card,
+    CardContent,
+    Grid,
+    Typography,
+    Skeleton,
+    Select,
+    MenuItem,
+    Box
+}
